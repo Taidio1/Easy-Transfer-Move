43c493e0ce75235ec9d57f956be767df4377eab3
Zuzanna <80766251+Taidio1@users.noreply.github.com>
2024-07-09 19:29:38 +0200

    upd

--- js/main.js
+++ js/main.js
@@ -11,14 +11,7 @@ jQuery(document).ready(function($) {
 
 	"use strict";
 	
-	window.addEventListener('load', () => {
-		AOS.init({
-		duration: 1000,
-		easing: 'ease-in-out',
-		once: true,
-		mirror: false
-		})
-	});
+
 	
 
 	var siteMenuClone = function() {
